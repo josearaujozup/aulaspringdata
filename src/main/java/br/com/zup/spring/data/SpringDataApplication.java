4018924a779d63d6379e9eeb932f2d1e0bfccf52
Denes <jose.araujo@zup.com.br>
2022-03-21 16:39:52 -0300

    Configuracao de propriedades para funcionar o spring jpa. Criacao do CRUD da entidade cargo

--- src/main/java/br/com/zup/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/zup/spring/data/SpringDataApplication.java
@@ -1,13 +1,50 @@
 package br.com.zup.spring.data;
 
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringDataApplication {
+import br.com.zup.spring.data.orm.Cargo;
+import br.com.zup.spring.data.repository.CargoRepository;
+import br.com.zup.spring.data.service.CrudCargoService;
 
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner{
+	
+	private final CrudCargoService cargoService;
+	
+	private Boolean system = true;
+	
+	public SpringDataApplication(CrudCargoService cargoService) {
+		this.cargoService = cargoService;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		
+		while(system) {
+			System.out.println("Qual acao voce quer executar");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+			
+			int action = scanner.nextInt();
+			if(action == 1) {
+			cargoService.inicial(scanner);	
+			}else {
+				system = false;
+			}
+		}
+		
+//		Cargo cargo = new Cargo();
+//		cargo.setDescricao("Desenvolvedor de software");
+//		repository.save(cargo);
 	}
 
 }
